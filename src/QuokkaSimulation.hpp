85f79e223632fbc081355098ea4f007bd160ca38
ChongChong He <chongchong.he@anu.edu.au>
2024-12-18 17:52:19 +1100

    minor

--- src/QuokkaSimulation.hpp
+++ src/QuokkaSimulation.hpp
@@ -295,11 +295,10 @@ template <typename problem_t> void QuokkaSimulation<problem_t>::PrintRadEnergySo
 		auto const &radEnergySource_arr = radEnergySource.array(iter);
 		amrex::ParallelFor(indexRange, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
 			// amrex::Print() << radEnergySource_arr(i, j, k) << ", ";
-			if (k == 16) {
+			if (k == 31 && i == 15 && j == 31) {
 				amrex::Print() << "(" << i << ", " << j << ", " << k << "): " << radEnergySource_arr(i, j, k) << "\n";
 			}
 		});
-		amrex::Print() << "\n";
 	}
 }
 
