1d71046043082179ce051e4bc6ccc8a0e498c4fe
ChongChong He <chongchong.he@anu.edu.au>
2024-12-18 13:04:12 +1100

    GravRadParticle3D: only radiation

---
+++ src/problems/GravRadParticle3D/test_grav_rad_particle_3D.hpp
@@ -0,0 +1,19 @@
+#ifndef TEST_GRAV_RAD_PARTICLE_3D_HPP_ // NOLINT
+#define TEST_GRAV_RAD_PARTICLE_3D_HPP_
+/// \file test_grav_rad_particle_3D.hpp
+/// \brief Defines a 3D test problem for radiating particles with gravity.
+///
+
+// external headers
+#ifdef HAVE_PYTHON
+#include "util/matplotlibcpp.h"
+#endif
+#include <fmt/format.h>
+
+// internal headers
+
+#include "radiation/radiation_system.hpp"
+
+// function definitions
+
+#endif // TEST_GRAV_RAD_PARTICLE_3D_HPP_
