f83f9f63bfa60e1d1d7f49aef5fe8327b5601cd3
ChongChong He <chongchong.he@anu.edu.au>
2024-12-22 18:42:25 +1100

    Redistribute particles after particle drift

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -949,6 +949,20 @@ template <typename problem_t> void AMRSimulation<problem_t>::evolve()
 		// N.B.: MUST be done *before* Poisson solve at new time!
 		driftParticlesAllLevels(dt_[0]);
 
+#ifdef AMREX_PARTICLES
+		// Redistribute particles after movement. This ensures particles are in the correct cells/processors for mass deposition in ellipticSolveAllLevels()
+		if (do_tracers != 0) {
+			for (int lev = 0; lev <= finest_level; ++lev) {
+				TracerPC->Redistribute(lev);
+			}
+		}
+		if (do_cic_particles != 0) {
+			for (int lev = 0; lev <= finest_level; ++lev) {
+				CICParticles->Redistribute(lev);
+			}
+		}
+#endif
+
 		// elliptic solve over entire AMR grid (post-timestep)
 		ellipticSolveAllLevels(dt_[0]);
 
