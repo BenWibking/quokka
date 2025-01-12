9617c05c05932dbe6a756dfeb1b3edd43b12504d
ChongChong He <chongchong.he@anu.edu.au>
2025-01-12 12:17:47 +1100

    cleanup

--- src/problems/RadCoolingMG/test_radiation_cooling_MG.hpp
+++ src/problems/RadCoolingMG/test_radiation_cooling_MG.hpp
@@ -1,10 +1,5 @@
 #ifndef TEST_RADIATION_COOLING_MG_HPP_ // NOLINT
 #define TEST_RADIATION_COOLING_MG_HPP_
-//==============================================================================
-// TwoMomentRad - a radiation transport library for patch-based AMR codes
-// Copyright 2020 Benjamin Wibking.
-// Released under the MIT license. See LICENSE file included in the GitHub repo.
-//==============================================================================
 /// \file test_radiation_cooling_MG.hpp
 /// \brief Defines a test problem for radiation in the free-streaming regime.
 ///
