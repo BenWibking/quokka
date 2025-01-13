b0a5d15c9108f63af8cae6f2ea2a0d446c4bd490
ChongChong He <chongchong.he@anu.edu.au>
2025-01-13 14:27:19 +1100

    add do_midpoint_convergence_check

--- src/radiation/radiation_dust_system.hpp
+++ src/radiation/radiation_dust_system.hpp
@@ -463,17 +463,19 @@ AMREX_GPU_DEVICE auto RadSystem<problem_t>::SolveGasDustRadiationEnergyExchange(
 		if (std::abs(jacobian.F0 / Etot0) < resid_tol && cscale * jacobian.Fg_abs_sum / Etot0 < resid_tol) {
 			break;
 		}
-		// check convergence of the average of two consecutive iterations
-		if (n > 0 && n % 2 == 0) {
-			Egas_mid_prev = Egas_mid;
-			Erad_mid_prev = Erad_mid;
-			Egas_mid = 0.5 * (Egas_prev + Egas_guess);
-			Erad_mid = 0.5 * (Erad_prev + EradVec_guess);
-			if (std::abs(jacobian.F0 / Etot0) < resid_tol && std::abs(Egas_mid - Egas_mid_prev) < mid_tol * Etot0 &&
-			    cscale * max(abs(Erad_mid - Erad_mid_prev)) < mid_tol * Etot0) {
-				// for debugging
-				// amrex::Print() << "Converged at n = " << n << " via mid-point method.\n";
-				break;
+		if constexpr (do_midpoint_convergence_check) {
+			// check convergence of the average of two consecutive iterations
+			if (n > 0 && n % 2 == 0) {
+				Egas_mid_prev = Egas_mid;
+				Erad_mid_prev = Erad_mid;
+				Egas_mid = 0.5 * (Egas_prev + Egas_guess);
+				Erad_mid = 0.5 * (Erad_prev + EradVec_guess);
+				if (std::abs(jacobian.F0 / Etot0) < resid_tol && std::abs(Egas_mid - Egas_mid_prev) < mid_tol * Etot0 &&
+						cscale * max(abs(Erad_mid - Erad_mid_prev)) < mid_tol * Etot0) {
+					// for debugging
+					// amrex::Print() << "Converged at n = " << n << " via mid-point method.\n";
+					break;
+				}
 			}
 		}
 
@@ -835,17 +837,19 @@ AMREX_GPU_DEVICE auto RadSystem<problem_t>::SolveGasDustRadiationEnergyExchangeW
 		if (std::abs(jacobian.F0 / Etot0) < resid_tol && cscale * jacobian.Fg_abs_sum / Etot0 < resid_tol) {
 			break;
 		}
-		// check convergence of the average of two consecutive iterations
-		if (n > 0 && n % 2 == 0) {
-			Egas_mid_prev = Egas_mid;
-			Erad_mid_prev = Erad_mid;
-			Egas_mid = 0.5 * (Egas_prev + Egas_guess);
-			Erad_mid = 0.5 * (Erad_prev + EradVec_guess);
-			if (std::abs(jacobian.F0 / Etot0) < resid_tol && std::abs(Egas_mid - Egas_mid_prev) < mid_tol * Etot0 &&
-			    cscale * max(abs(Erad_mid - Erad_mid_prev)) < mid_tol * Etot0) {
-				// for debugging
-				// amrex::Print() << "Converged at n = " << n << " via mid-point method.\n";
-				break;
+		if constexpr (do_midpoint_convergence_check) {
+			// check convergence of the average of two consecutive iterations
+			if (n > 0 && n % 2 == 0) {
+				Egas_mid_prev = Egas_mid;
+				Erad_mid_prev = Erad_mid;
+				Egas_mid = 0.5 * (Egas_prev + Egas_guess);
+				Erad_mid = 0.5 * (Erad_prev + EradVec_guess);
+				if (std::abs(jacobian.F0 / Etot0) < resid_tol && std::abs(Egas_mid - Egas_mid_prev) < mid_tol * Etot0 &&
+						cscale * max(abs(Erad_mid - Erad_mid_prev)) < mid_tol * Etot0) {
+					// for debugging
+					// amrex::Print() << "Converged at n = " << n << " via mid-point method.\n";
+					break;
+				}
 			}
 		}
 
